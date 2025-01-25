c4545b3cbe47cd84a6bd6119d8b3459a53f7c608
Joni Uitto <joni.uitto@gmail.com>
2025-01-25 15:27:37 +0200

    Wondering why items do not populate

--- Assets/Datas.cs
+++ Assets/Datas.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
+using UnityEditor.Rendering.Universal;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+
 
 public enum UpgradeType
 {
@@ -15,7 +19,8 @@ public enum UpgradeType
 [Serializable]
 public class TurtleUpgrade
 {
-    private static readonly Dictionary<UpgradeType, float> pricePerType = new Dictionary<UpgradeType, float> {
+    private static readonly Dictionary<UpgradeType, float> pricePerType = new()
+    {
         { UpgradeType.CarryingCapacity, 5 },
         { UpgradeType.OxygenMaximum, 12 },
         { UpgradeType.Speed, 15 },
@@ -24,12 +29,35 @@ public class TurtleUpgrade
 
     public int level = 0;
     public UpgradeType type = UpgradeType.None;
+    public Sprite icon;
 
-    public float GetPrice()
+    [SerializeField]
+    public float price
     {
-        return level * pricePerType[type];
+        get { return level * pricePerType[type]; }
+        set { /* I do not actually care */ }
+    }
+
+    public static string LevelToString(int level)
+    {
+        return "lvl " + level.ToString();
+    }
+
+    internal static string TypeToString(UpgradeType type)
+    {
+        Debug.Log("Converting type");
+        switch (type)
+        {
+            case UpgradeType.None: return "BUG!!";
+            case UpgradeType.CarryingCapacity: return "Carrying capacity";
+            case UpgradeType.OxygenMaximum: return "Oxygen maximum";
+            case UpgradeType.Speed: return "Maximum speed";
+            case UpgradeType.Toughness: return "Toughness";
+        }
+        return "PANIC!!";
     }
 }
+
 
 [Serializable]
 public class TurtleInventory
@@ -46,7 +74,7 @@ public class TurtleData
     public float oxygen = 100f;
     public float maxOxygen = 100f;
     public float carryingCapacity = 100f;
-    public TurtleInventory inventory = new TurtleInventory();
+    public TurtleInventory inventory = new();
 }
 
 public enum CollectableType
@@ -68,5 +96,21 @@ public class CollectableData
 [Serializable]
 public class ShopData
 {
-    public List<TurtleUpgrade> items = new List<TurtleUpgrade>();
+    public List<TurtleUpgrade> items = new();
+    public TurtleInventory inventory = new();
+}
+
+
+public class UIConverters
+{
+    [UnityEditor.InitializeOnLoadMethod]
+    public static void InitConverters()
+    {
+        ConverterGroup g = new("shop");
+
+        g.AddConverter((ref int level) => TurtleUpgrade.LevelToString(level));
+        g.AddConverter((ref UpgradeType type) => TurtleUpgrade.TypeToString(type));
+
+        ConverterGroups.RegisterConverterGroup(g);
+    }
 }
--- Assets/Shop.cs
+++ Assets/Shop.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,11 +7,27 @@ using UnityEngine.UIElements;
 public class Shop : MonoBehaviour
 {
 
-    private ShopData sData = new ShopData();
+    private ShopData dShop = new();
+    private VisualElement root;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GetComponent<UIDocument>().rootVisualElement.dataSource = sData;
+        root = GetComponent<UIDocument>().rootVisualElement.Children().First();
+
+        for (int i = 0; i < 2; i++)
+        {
+            TurtleUpgrade u = new()
+            {
+                level = 1,
+                type = UpgradeType.Speed
+            };
+
+            dShop.items.Add(u);
+        }
+        root.dataSource = dShop.items;
+
+        Debug.Log(root.name);
     }
 
     // Update is called once per frame
