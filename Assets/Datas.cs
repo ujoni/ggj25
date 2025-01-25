f68706fc3f002488dfb9f2d7b5212e3a1a1c64ec
Joni Uitto <joni.uitto@gmail.com>
2025-01-25 12:33:44 +0200

    Starting to build the shop

--- Assets/Datas.cs
+++ Assets/Datas.cs
@@ -1,5 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
+
+public enum UpgradeType
+{
+    None,
+    OxygenMaximum,
+    Speed,
+    Toughness,
+    CarryingCapacity,
+}
+
+[Serializable]
+public class TurtleUpgrade
+{
+    private static readonly Dictionary<UpgradeType, float> pricePerType = new Dictionary<UpgradeType, float> {
+        { UpgradeType.CarryingCapacity, 5 },
+        { UpgradeType.OxygenMaximum, 12 },
+        { UpgradeType.Speed, 15 },
+        { UpgradeType.Toughness, 6 },
+    };
+
+    public int level = 0;
+    public UpgradeType type = UpgradeType.None;
+
+    public float GetPrice()
+    {
+        return level * pricePerType[type];
+    }
+}
+
+[Serializable]
+public class TurtleInventory
+{
+    public TurtleUpgrade[] inventory = new TurtleUpgrade[13];
+}
 
 [Serializable]
 public class TurtleData
@@ -9,6 +45,8 @@ public class TurtleData
     public float depth = 0;
     public float oxygen = 100f;
     public float maxOxygen = 100f;
+    public float carryingCapacity = 100f;
+    public TurtleInventory inventory = new TurtleInventory();
 }
 
 public enum CollectableType
@@ -25,4 +63,10 @@ public enum CollectableType
 public class CollectableData
 {
     public CollectableType collectableType = CollectableType.None;
+}
+
+[Serializable]
+public class ShopData
+{
+    public List<TurtleUpgrade> items = new List<TurtleUpgrade>();
 }
